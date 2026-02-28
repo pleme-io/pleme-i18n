8fe794dee773755a0252b6c1f20b78beeb5e9e2a
drzzln <drzzln@protonmail.com>
2026-02-27 21:12:09 -0500

    chore: switch nixpkgs to nixos-25.11 stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Internationalization wrapper for react-i18next with Brazilian Portuguese support";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     dream2nix = {
       url = "github:nix-community/dream2nix";
       inputs.nixpkgs.follows = "nixpkgs";
